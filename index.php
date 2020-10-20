753bd2ce0b50bf7c8ecb7f0b9fab31d37136f54e
Georgii Sirotkin <georgiysirotkin@gmail.com>
2020-10-19 20:05:06 -0400

    Add header and footer partials. Add home page

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once('partials/header.php');
+?>
+    <section class="jumbotron text-center">
+        <div class="container">
+            <h1>Meet your next favourite book</h1>
+            <p class="lead text-muted">
+                Georgii's book store was created for Designing Effective E-Commerce Sites class.
+            </p>
+            <p>
+                <a class="btn btn-primary my-2 mr-1" href="list.php">
+                    Browse books
+                </a>
+            </p>
+        </div>
+    </section>
+<?php
+require_once('partials/footer.php');
---
+++ partials/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Georgii's book store</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container"><a class="navbar-brand" href="/">Georgii's book store</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="/list">Books </a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container mt-4">
